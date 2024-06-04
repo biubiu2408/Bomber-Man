7a2b2cb9303432adc216fa4b78ea5f25d1b97adc
Khanh Nguyen <113000662+biubiu2408@users.noreply.github.com>
2024-06-04 22:50:16 +0700

    Add files via upload

--- scr/index.js
+++ scr/index.js
@@ -1,1 +1,26 @@
+import { BomberManGame } from 'game/BomberManGame.js';
+
+// window.addEventListener('load', () => {
+// 	new BomberManGame().start();
+// });
+
+// let game = new BomberManGame();
+const advanture = document.getElementById('adventure');
+const battle = document.getElementById('battle');
+
+battle.addEventListener('click', function () {
+	document.querySelector('.menu').style.display = 'none';
+	new BomberManGame(0).start();
+	// game.start();
+	// alert("ok 1");
+});
+
+advanture.addEventListener('click', function () {
+	document.querySelector('.menu').style.display = 'none';
+	new BomberManGame(1).start();
+	// game.start();
+	// alert("ok 2");
+});
+
+
 

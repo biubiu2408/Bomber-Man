5521162fb5f0c63db22c18c9654178228162c59e
Khanh Nguyen <113000662+biubiu2408@users.noreply.github.com>
2024-06-04 22:54:39 +0700

    Add files via upload

---
+++ scr/game/constants/game.js
@@ -0,0 +1,38 @@
+/**
+ * Set up you main game constant values here
+ */
+// import { player, win } from '../utils/htmlValue.js'
+export const FPS = 60;
+export const FRAME_TIME = 1000 / FPS;
+
+export const SCREEN_WIDTH = 256;
+export const SCREEN_HEIGHT = 224;
+
+export const TILE_SIZE = 16;
+export const HALF_TILE_SIZE = TILE_SIZE / 2;
+
+export const STAGE_OFFSET_Y = 24;
+
+export let NO_ENEMY = 2;
+export let NO_PLAYERS = 2;
+export let MAX_WINS = 1;
+// if (player !== 0) NO_PLAYERS = player;
+// if (win !== 0) MAX_WINS = win;
+let form = document.forms["my-form"];
+if (form) {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // setPlayer(this.player.value);
+    // setWin(this.win.value);
+    // console.log(n_win);
+    // console.log(n_player);
+    NO_PLAYERS = this.player.value;
+    MAX_WINS = this.win.value;
+    console.log(NO_PLAYERS);
+    console.log(MAX_WINS);
+  });
+}
+console.log(NO_PLAYERS)
+// console.log(player)
+export const DEBUG = false;
